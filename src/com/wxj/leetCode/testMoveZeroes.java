05ea0fddd2ed539ddf6dc5e397f1270db2dd66d5
wxj01 <269377151@qq.com>
2021-05-08 18:36:05 +0800

    移动零

--- src/com/wxj/leetCode/testMoveZeroes.java
+++ src/com/wxj/leetCode/testMoveZeroes.java
@@ -30,7 +30,8 @@ package com.wxj.leetCode;
 public class testMoveZeroes {
     public static void main(String[] args) {
         int [] nums = {0,1,0,3,12};
-        moveZeroes(nums);
+//        moveZeroes(nums);
+        moveZeroes2(nums);
     }
 
     public static void moveZeroes(int[] nums) {
@@ -55,4 +56,28 @@ public class testMoveZeroes {
 
 
     }
+
+//    参照双指针解决
+    /**
+     指针j是一直往后移动的，如果指向的值不等于0才对他进行操作。而i统计的是前面0的个数，我们可以把j-i看做另一个指针，它是指向前面第一个0的位置，然后我们让j指向的值和j-i指向的值交换
+
+     作者：数据结构和算法
+     链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2ba4i/?discussion=AJ2rEF
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @date: 2021/5/8 0008 18:14
+     */
+    public static void moveZeroes2(int[] nums) {
+        int i = 0; // 统计前面 0 的个数
+        for (int j = 0; j < nums.length;j++){
+            if(nums[j] == 0){
+                i++;
+            }else if(i != 0){
+                // 否则，把当前数字放到最前面那个0 的位置，然后再把 当前位置设置为 0
+                nums[j-i] = nums[j];
+                nums[j] = 0;
+            }
+        }
+    }
+
 }
