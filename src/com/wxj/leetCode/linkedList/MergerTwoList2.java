fcb789496407a8878345dfc9b09efbdb124fc23c
wxj01 <wangxinjian_01@163.com>
2021-06-17 00:21:24 +0800

    刷题

---
+++ src/com/wxj/leetCode/linkedList/MergerTwoList2.java
@@ -0,0 +1,58 @@
+package com.wxj.leetCode.linkedList;
+
+/**
+ *    两个链表 一个升序 一个降序，将合并一个有序的链表
+ *
+ */
+public class MergerTwoList2 {
+    class Node{
+        int val;
+        Node next;
+    }
+
+    // 反转链表
+    private Node reverList(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        Node dummy = null;
+        Node cur = head;
+        Node next = head;
+        while (cur != null){
+            next = next.next;
+            cur.next = dummy;
+            dummy = cur;
+            cur = next;
+        }
+
+        return dummy;
+    }
+
+    private Node megeList(Node h1,Node h2){
+
+        if(h1 == null){
+            return h2;
+        }
+
+        if(h2 == null){
+            return h1;
+        }
+
+        Node dummy = new Node();
+        Node cur = dummy;
+
+        while (h1 != null && h2 != null){
+            if(h1.val < h2.val){
+                cur.next = h1;
+                h1 = h1.next;
+            }else {
+                cur.next = h2;
+                h2 = h2.next;
+            }
+
+            cur = cur.next;
+        }
+        return dummy;
+    }
+}
