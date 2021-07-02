fcab23cffaf867772d913e4d38869b19eb76556f
wxj01 <269377151@qq.com>
2021-07-02 16:10:47 +0800

    链表

---
+++ src/com/wxj/leetCode/testGetIntersectionNode.java
@@ -0,0 +1,33 @@
+package com.wxj.leetCode;
+
+/**
+ * @author wxj
+ * @version 1.0
+ * @description: TODO
+ *
+ * 给定两个（单向）链表，判定它们是否相交并返回交点。请注意相交的定义基于节点的引用，而不是基于节点的值。
+ * 换句话说，如果一个链表的第k个节点与另一个链表的第j个节点是同一节点（引用完全相同），则这两个链表相交。
+ *
+ *
+ * @date 2021/6/7 0007 15:46
+ */
+public class testGetIntersectionNode {
+
+
+//      Definition for singly-linked list.
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+            return null;
+
+    }
+
+}
