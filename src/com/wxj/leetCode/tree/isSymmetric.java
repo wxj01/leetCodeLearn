fe36d8037a993690f997af5d44a62c9ec452adc1
wxj01 <wangxinjian_01@163.com>
2021-07-15 21:56:07 +0800

    给定一个二叉树，检查它是否是镜像对称的。

--- src/com/wxj/leetCode/tree/isSymmetric.java
+++ src/com/wxj/leetCode/tree/isSymmetric.java
@@ -7,18 +7,7 @@ package com.wxj.leetCode.tree;
 /**
  * Definition for a binary tree node.
  * */
-class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+
 
 public class isSymmetric {
     public boolean isSymmetric(TreeNode root) {
