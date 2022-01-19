317ee1b07a74d0ea095825e01c76a4163ee2920e
wxj_2021 <wangxinjian_01@163.com>
2022-01-19 17:13:42 +0800

    两个数相加

--- src/com/wxj/leetCode/leetcode/editor/cn/AddTwoNumbers.java
+++ src/com/wxj/leetCode/leetcode/editor/cn/AddTwoNumbers.java
@@ -61,9 +61,50 @@ public class AddTwoNumbers {
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/**
+ * @description:
+ *
+    将当前结点初始化为返回列表的哑结点。
+    将进位 carrycarrycarry 初始化为 0。
+    将 p 和 q 分别初始化为列表 l1 和 l2 的头部。
+    遍历列表 l1 和 l2直至到达它们的尾端。
+    将 x 设为结点 p 的值。如果 ppp 已经到达 l1 的末尾，则将其值设置为 0。
+    将 y 设为结点 q 的值。如果 qqq 已经到达 l2 的末尾，则将其值设置为 0。
+    设定 sum=x+y+carry。
+    更新进位的值，carry=sum/10。
+    创建一个数值为 (sum mod 10) 的新结点，并将其设置为当前结点的下一个结点，然后将当前结点前进到下一个结点。
+    同时，将 p 和 q 前进到下一个结点。
+    检查 carry=1是否成立，如果成立，则向返回列表追加一个含有数字 1 的新结点。
+    返回哑结点的下一个结点。
+
+ *
+ * @author wangxinjian
+ * @date 2022/1/19 0019 16:14
+ * @version 1.0
+ */
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null){
 
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum =  x + y + carry;
+            carry = sum / 10 ;
+            curr.next = new ListNode(sum % 10 );
+            curr = curr.next;
+            if(p != null){ p = p.next; }
+            if(q != null){ q = q.next; }
+        }
+        if(carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
