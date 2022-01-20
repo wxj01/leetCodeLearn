88344bcce3d2ed0b1d18cf56e00167fad2712384
wxj_2021 <wangxinjian_01@163.com>
2022-01-20 13:31:36 +0800

    无重复字符的最大子字符串 解法2

--- src/com/wxj/leetCode/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/wxj/leetCode/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -51,7 +51,9 @@
 
 package com.wxj.leetCode.leetcode.editor.cn;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
@@ -62,16 +64,14 @@ public class LongestSubstringWithoutRepeatingCharacters {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0;
-        while ( i < n && j < n){
-            if(!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                ans = Math.max(ans,j - i);
-            }else {
-                set.remove(s.charAt(i++));
+      int n = s.length(), ans = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        for (int j = 0,i = 0; j < n ; j++){
+            if (map.containsKey(s.charAt(j))){
+                i = Math.max(map.get(s.charAt(j)),i);
             }
+            ans = Math.max(ans,j - i +1);
+            map.put(s.charAt(j),j+1);
         }
         return ans;
     }
