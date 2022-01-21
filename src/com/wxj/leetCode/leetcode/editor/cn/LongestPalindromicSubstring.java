d254d3da97043b9ca35e2c2070f4c2bed40fc88a
wxj_2021 <wangxinjian_01@163.com>
2022-01-21 20:26:01 +0800

    最长回文子串

---
+++ src/com/wxj/leetCode/leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -0,0 +1,85 @@
+/**
+  * 题目Id：5
+  * 题目：最长回文子串
+  * 日期：2022-01-20 20:23:47
+*/
+//给你一个字符串 s，找到 s 中最长的回文子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "a"
+//输出："a"
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：s = "ac"
+//输出："a"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母（大写和/或小写）组成 
+// 
+// Related Topics 字符串 动态规划 👍 4596 👎 0
+
+package com.wxj.leetCode.leetcode.editor.cn;
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution solution = new LongestPalindromicSubstring().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() < 1){
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s,i,i);
+            int len2 = expandAroundCenter(s,i,i+1);
+            int len = Math.max(len1,len2);
+            if (len > end - start){
+                start = i - (len -1)/2 ;
+                end = i + len/2;
+            }
+        }
+        return s.substring(start,end + 1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right){
+        while ( left > 0 && right < s.length()
+                && s.charAt(left) ==  s.charAt(right)){
+            --left;
+            ++right;
+        }
+        return right - left -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
