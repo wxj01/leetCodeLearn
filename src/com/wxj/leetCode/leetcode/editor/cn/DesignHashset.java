055b6a1ef45855efa2f6fa594518ea5fe203e29c
wxj_2021 <wangxinjian_01@163.com>
2022-02-07 16:14:07 +0800

    [705]设计哈希集合

--- src/com/wxj/leetCode/leetcode/editor/cn/DesignHashset.java
+++ src/com/wxj/leetCode/leetcode/editor/cn/DesignHashset.java
@@ -46,6 +46,9 @@
 // Related Topics 设计 数组 哈希表 链表 哈希函数 👍 214 👎 0
 
 package com.wxj.leetCode.leetcode.editor.cn;
+
+import org.w3c.dom.Node;
+
 public class DesignHashset {
     public static void main(String[] args) {
         Solution solution = new DesignHashset().new Solution();
@@ -54,22 +57,93 @@ public class DesignHashset {
     //leetcode submit region begin(Prohibit modification and deletion)
 class MyHashSet {
 
-    boolean[] nodes = new boolean[1000001];
+    // 由于使用的是「链表」，这个值可以取得很小
+    Node[] nodes = new Node[10001];
 
     public MyHashSet() {
 
     }
     
     public void add(int key) {
-        nodes[key] = true;
+        // 根据 key 获取哈希桶的位置
+        int idx = getIndex(key);
+        // 判断链表中是否已经存在
+        Node loc = nodes[idx],tmp = loc;
+        if(loc != null){
+            Node prev = null;
+            while (tmp != null){
+                if(tmp.key == key){
+                    return;
+                }
+                prev = tmp;
+                tmp = tmp.next;
+            }
+            tmp = prev;
+        }
+        Node node = new Node(key);
+        //头插法
+//        node.next = loc;
+//        node[idx] =  node;
+
+        //尾插法
+        if (tmp != null){
+            tmp.next = node;
+        }else {
+            nodes[idx] = node;
+        }
     }
     
     public void remove(int key) {
-        nodes[key] = false;
+        int idx = getIndex(key);
+        Node loc = nodes[idx];
+        if (loc != null) {
+            Node prev = null;
+            while (loc != null) {
+                if (loc.key == key) {
+                    if (prev != null) {
+                        prev.next = loc.next;
+                    }else {
+                        nodes[idx] = loc.next;
+                    }
+                    return;
+                }
+                prev = loc;
+                loc = loc.next;
+            }
+        }
     }
     
     public boolean contains(int key) {
-        return nodes[key];
+        int idx = getIndex(key);
+        Node loc = nodes[idx];
+        if (loc != null) {
+            while (loc != null) {
+                if (loc.key == key) {
+                    return true;
+                }
+                loc = loc.next;
+            }
+        }
+        return false;
+    }
+
+
+    static class Node{
+        private int key;
+        private Node next;
+
+        public Node(int key) {
+            this.key = key;
+        }
+    }
+
+    int getIndex(int key){
+        // 因为 nodes 的长度只有 10009，对应的十进制的 10011100010001（总长度为 32 位，其余高位都是 0）
+        // 为了让 key 对应的 hash 高位也参与运算，这里对 hashCode 进行右移异或
+        // 使得 hashCode 的高位随机性和低位随机性都能体现在低 16 位中
+        int hash = Integer.hashCode(key);
+        hash ^= hash>>>16;
+        return hash % nodes.length;
     }
 }
 
